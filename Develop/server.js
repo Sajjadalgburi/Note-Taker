00403b4ffb56293e02a5eca266cd66c8e7d63070
Sajjadalgburi <dfgsajjad@gmail.com>
2024-02-06 15:02:16 -0700

    initilizing basic express server

--- Develop/server.js
+++ Develop/server.js
@@ -1,4 +1,23 @@
+const express = require("express");
 const app = express();
 const path = require("path");
 const PORT = process.env.PORT || 3001;
 const api = require("./routes/index.js");
+
+// Middleware for static assets
+app.use(express.static("public"));
+
+// Define html route for notes
+app.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "public/notes.html"));
+});
+
+// Define catch-all route to return index.html
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "public/homepage.html"));
+});
+
+// calling the server through listen method
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
