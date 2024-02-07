2629d5e81ebeedb1aa8e6d0846e7c7b26af6a07a
Sajjadalgburi <dfgsajjad@gmail.com>
2024-02-07 13:12:46 -0700

    writing code for the appropriate note routes

---
+++ Develop/routes/notes.js
@@ -0,0 +1,35 @@
+const notes = require("express").Router();
+const uuid = require("../helpers/uuid");
+
+// Helper functions for reading and writing to the JSON file
+const {
+  readFromFile,
+  writeToFile,
+  readAndAppend,
+} = require("../helpers/fsUtils");
+
+notes.get("/", (req, res) => {
+  console.info(`${req.method} request received for notes`);
+  readFromFile("./db/db.json").then((data) => res.json(JSON.parse(data)));
+});
+
+notes.post("/", (req, res) => {
+  console.info(`${req.method} request received to add a new note`);
+
+  const { title, text } = req.body;
+
+  if (req.body) {
+    const newNote = {
+      title,
+      text,
+      tip_id: uuid(),
+    };
+
+    readAndAppend(newNote, "./db/db.json");
+    res.json(`Tip added successfully 🚀`);
+  } else {
+    res.error("Error in adding tip");
+  }
+});
+
+module.exports = notes;
