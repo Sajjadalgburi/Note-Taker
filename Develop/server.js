ef1a3e4b794697eb3fa5f61619e38ac55bea7276
Sajjadalgburi <dfgsajjad@gmail.com>
2024-02-07 13:10:20 -0700

    added the correct middlewares

--- Develop/server.js
+++ Develop/server.js
@@ -2,18 +2,27 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const PORT = process.env.PORT || 3001;
-const api = require("./routes/index.js");
+const api = require("./routes/main");
 
 // Middleware for static assets
 app.use(express.static("public"));
+
+// Middleware for parsing JSON and urlencoded form data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// created a costume route for GET and POST /notes
+app.use("/api", api);
 
 // Define html route for notes
 app.get("/notes", (req, res) => {
   res.sendFile(path.join(__dirname, "public/notes.html"));
 });
 
-// Define catch-all route to return index.html
+// Define catch-all route to return homepage.html
 app.get("*", (req, res) => {
+  console.info(`${req.method} request received for homepage`);
+
   res.sendFile(path.join(__dirname, "public/homepage.html"));
 });
 
